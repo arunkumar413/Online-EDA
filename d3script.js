0ed06344c1372c3a13bae0d55a375c1ddb0c9a00
Arun <arunkumar413@gmail.com>
2016-08-25 21:41:40 +0530

    made changes to an IC

--- d3script.js
+++ d3script.js
@@ -3,14 +3,68 @@ $( document ).ready(function() {
   // Your d3 code here
 
 
-var mysvg = d3.select("svg")
- .attr("width", 500)
-    .attr("height", 500);
+//var mysvg = d3.select("svg")
+ //.attr("width", 500)
+   // .attr("height", 500);
     
 
-var img = mysvg.append("svg:image")
-    .attr("xlink:href", "/IC-*pin.svg");
+//var img = mysvg.append("svg:image")
+   // .attr("xlink:href", "/IC-8pin.svg"); //
 	
     	
+		    $("#mybut").click(add_part);
+			$("#wire").click(add_wire);
+			//$("#remove").click(remove_part);
+
+			
+
+			
+			
+			
+			
+			function add_part ()
+			{
+d3.select("svg")
+.append("object")
+.attr("data", "/parts/IC-8pin.svg")
+.attr("type", "image/svg+xml");
+			
+			
+			} //end of add_part
+	
+		function add_wire()
+		 {
+			 
+			 
+			 
+		 } //end of add_wire
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		});
