f85687c3595673c22189481cc48908edc874d82e
Arun <arunkumar413@gmail.com>
2016-08-25 13:28:04 +0530

    new parts added

---
+++ d3script.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+  // Your d3 code here
+
+
+var mysvg = d3.select("svg")
+ .attr("width", 500)
+    .attr("height", 500);
+    
+
+var img = mysvg.append("svg:image")
+    .attr("xlink:href", "/IC-*pin.svg");
+	
+    	
+		
+		});
